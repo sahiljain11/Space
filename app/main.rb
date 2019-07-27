802464476c01846868ce408dade495d3ee17fe28
Sahil Jain <sahilbot1@gmail.com>
2019-07-27 18:37:26 -0500

    Added static background. Switch players with 1, 2, and 3 keys

--- app/main.rb
+++ app/main.rb
@@ -9,33 +9,46 @@ class Prototype
     end
 
     def defaults
-        state.playerControlCord ||= [590, 230]
         state.currentPlayer ||= 1
+        state.walkingSpeed  ||= 4
 
         state.players ||= [
-            [:playerOne,   590, 230, 1],
-            [:playerTwo,     0,   0, 2],
-            [:playerThree, 800, 200, 3]
+            [:playerOne,  4590, 230, 1],
+            [:playerTwo,  4000,   0, 2],
+            [:playerThree,4800, 200, 3]
         ].map do |name, x, y, order|
             state.new_entity(name) do |p|
                 p.name = name
                 p.absolutex = x
                 p.absolutey = y
-                p.relativex = x - state.playerControlCord[0]
-                p.relativey = y - state.playerControlCord[1]
                 p.order = order
                 p.sprite = "sprites/player" + order.to_s + ".png"
             end
         end
+
+        state.playerControlCord = [state.players[state.currentPlayer - 1].absolutex, state.players[state.currentPlayer - 1].absolutey]
+
     end
 
     def render
+        state.deltaX = state.playerControlCord[0] - 590
+        state.deltaY = state.playerControlCord[1] - 230
         outputs.sprites += state.players.map {
             |p|
-            [p.absolutex, p.absolutey, 75, 150, p.sprite]
+            if render?(p.absolutex, p.absolutey)
+                [p.absolutex - state.deltaX, p.absolutey - state.deltaY, 75, 150, p.sprite]
+            else
+                []
+            end
         }
+    end
 
-
+    def render? x, y
+        if x >= state.playerControlCord[0] - 590 && x <= state.playerControlCord[0] + 690 &&
+           y >= state.playerControlCord[1] - 230 && y <= state.playerControlCord[1] + 490
+            return true
+        end
+        return false
     end
 
     def calc
@@ -47,23 +60,31 @@ class Prototype
             $dragon.reset
         end
 
-        if inputs.keyboard.key_up.1 && state.currentPlayer != 1
-            transition(1)
-        end
+        transitionPlayer(1) if inputs.keyboard.key_up.one   && state.currentPlayer != 1
+        transitionPlayer(2) if inputs.keyboard.key_up.two   && state.currentPlayer != 2
+        transitionPlayer(3) if inputs.keyboard.key_up.three && state.currentPlayer != 3
 
+        state.players[state.currentPlayer - 1].absolutex += state.walkingSpeed if inputs.keyboard.key_down.d
+        state.players[state.currentPlayer - 1].absolutex -= state.walkingSpeed if inputs.keyboard.key_down.a
+        state.players[state.currentPlayer - 1].absolutey += state.walkingSpeed if inputs.keyboard.key_down.w
+        state.players[state.currentPlayer - 1].absolutey -= state.walkingSpeed if inputs.keyboard.key_down.s
+        
     end
 
-    def transition n
+    def transitionPlayer n
         state.currentPlayer = n
-        state.playerControlCord = [state.players[n - 1].absolutex, state.players[n - 1].absolutey]
+
+        state.deltaX = state.players[n - 1].absolutex - state.playerControlCord[0]
+        state.deltaY = state.players[n - 1].absolutey - state.playerControlCord[1]
 
         state.players.map {
             |p|
-            unless p.order == n
-              p.absolutex = p.x + state.playerControlCord[0]    #Double check logic plz future me
-              p.absolutey = p.y + state.playerControlCord[1]
-            end
+            p.absolutex -= state.deltaX
+            p.absolutey -= state.deltaY
         }
+
+        state.playerControlCord[0] = state.players[n - 1].absolutex
+        state.playerControlCord[1] = state.players[n - 1].absolutey
 
     end
   
@@ -77,4 +98,3 @@ def tick args
     $prototype.outputs = args.outputs
     $prototype.tick
 end
-
